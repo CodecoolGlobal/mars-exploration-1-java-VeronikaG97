484b70de2bed491064f3384b24fd1d1ae9aa1175
Bernadette Christiner <b.christiner93@gmail.com>
2023-02-28 19:46:45 +0100

    Create new branch

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -6,5 +6,6 @@ public class Application {
     public static void main(String[] args) {
         Random random = new Random();
         System.out.println(random.nextInt(42));
+        System.out.println("New branch");
     }
 }

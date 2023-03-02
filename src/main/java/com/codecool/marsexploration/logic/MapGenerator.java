0bf162d1ccd32fc612261de11df29c62e91a458c
VeronikaG97 <109737441+VeronikaG97@users.noreply.github.com>
2023-03-02 10:58:37 +0100

    added strategies

--- src/main/java/com/codecool/marsexploration/logic/MapGenerator.java
+++ src/main/java/com/codecool/marsexploration/logic/MapGenerator.java
@@ -1,7 +1,11 @@
 package com.codecool.marsexploration.logic;
 
+import com.codecool.marsexploration.data.MapElement;
 import com.codecool.marsexploration.logic.MountainMap.MountainMapGenerator;
 import com.codecool.marsexploration.logic.PitsMap.PitsMapGenerator;
+
+import java.util.List;
+import java.util.Map;
 
 public class MapGenerator {
     private final String size;
@@ -15,8 +19,8 @@ public class MapGenerator {
     }
 
     public void generateMap() {
-        mountainMapGenerator.createMountainsMap(size);
-        pitsMapGenerator.createPitsMap(size);
+        List<MapElement> mountainElements = mountainMapGenerator.createMountainsMap(size);
+        List<MapElement> pitElements = pitsMapGenerator.createPitsMap(size);
         //Validate
         //Generate finished whole Map
         //Write it to file

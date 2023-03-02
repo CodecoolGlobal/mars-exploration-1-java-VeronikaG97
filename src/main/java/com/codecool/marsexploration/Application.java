93c11b79af5767973e8f133605e06ad101ff9a38
VeronikaG97 <109737441+VeronikaG97@users.noreply.github.com>
2023-03-02 14:00:58 +0100

    worked on MapWriter

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -1,6 +1,7 @@
 package com.codecool.marsexploration;
 
 import com.codecool.marsexploration.logic.MapGenerator;
+import com.codecool.marsexploration.logic.MapWriter;
 import com.codecool.marsexploration.logic.MountainMap.MountainMapGenerator;
 import com.codecool.marsexploration.logic.PitsMap.PitsMapGenerator;
 import com.codecool.marsexploration.logic.userInput.GetUserInput;
@@ -8,12 +9,14 @@ import com.codecool.marsexploration.logic.userInput.GetUserInput;
 public class Application {
 
     public static void main(String[] args) {
-        /*Instanzen für userInteraction */
+        /*Instanzen für userInteraction
         GetUserInput getUserInput = new GetUserInput();
         String size = getUserInput.getUserInput();
         MountainMapGenerator mountainMapGenerator = new MountainMapGenerator();
         PitsMapGenerator pitsMapGenerator = new PitsMapGenerator();
         MapGenerator mapGenerator = new MapGenerator(size,mountainMapGenerator,pitsMapGenerator);
-        mapGenerator.generateMap();
+        mapGenerator.generateMap();*/
+        MapWriter mapWriter = new MapWriter();
+        mapWriter.write();
     }
 }
--- src/main/java/com/codecool/marsexploration/logic/MapWriter.java
+++ src/main/java/com/codecool/marsexploration/logic/MapWriter.java
@@ -1,20 +1,33 @@
 package com.codecool.marsexploration.logic;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
-public class WriteMap {
-    public void write(bigMap){
+public class MapWriter {
+    public void write(){
+        String bigMap[][] = new String[2][2];
+        bigMap[0][0] = "*";
+        bigMap[0][1] = "#";
+        bigMap[1][0] = "+";
+        bigMap[1][1] = "~";
         try {
-            return doWrite(bigMap);
-        } catch (RuntimeException err){
-
+            doWrite(bigMap);
+        } catch (RuntimeException | IOException err){
+            System.out.println("Something went wrong with saving the map, we are working on it!");
         }
     }
 
-    private void doWrite(bigMap) {
-        Files.write(Paths.get("map.txt"), bigMap.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+    private void doWrite(String[][] bigMap) throws IOException {
+        String finishedMap = Arrays.toString(bigMap);
+        Files.write(Paths.get("src/main/resources/map.txt"),
+                finishedMap.getBytes(),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE
+        );
         System.out.println("Saved the map in: map.txt");
     }
 }

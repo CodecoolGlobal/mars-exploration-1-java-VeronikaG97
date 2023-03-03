77e37d7a708c1d4f89daca4971684d6ec1e1fc23
Bernadette Christiner <b.christiner93@gmail.com>
2023-03-03 12:17:04 +0100

    No relevant changes

--- src/test/java/com/codecool/marsexploration/logic/MountainMap/MountainMapGeneratorTest.java
+++ src/test/java/com/codecool/marsexploration/logic/MountainMap/MountainMapGeneratorTest.java
@@ -1,6 +1,15 @@
 package com.codecool.marsexploration.logic.MountainMap;
 
+import com.codecool.marsexploration.logic.CreateMap;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MountainMapGeneratorTest {
-  
+
 }
